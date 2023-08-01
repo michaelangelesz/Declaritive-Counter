ba72673409b0af85f6dc43b318546096ca893e0a
michaelangelesz <125311967+michaelangelesz@users.noreply.github.com>
2023-07-31 22:24:40 -0400

    add counter "echo" and margin

--- src/App.js
+++ src/App.js
@@ -6,7 +6,12 @@ function App() {
   return (
     <div>
       <h1>{counter}</h1>
+      <hr />
+      <h2>{counter}</h2>
+      <h3>{counter}</h3>
       <h4>{counter}</h4>
+      <h5>{counter}</h5>
+      <h6>{counter}</h6>
     </div>
   )
 }
